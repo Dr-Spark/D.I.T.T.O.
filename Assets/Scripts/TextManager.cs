39218dcfe5ae6fb417ab96653a4af55592c7e558
Ata <43355239+Dr-Spark@users.noreply.github.com>
2020-04-03 03:58:45 -0500

    KMD Update
    Lots, of stuff I don't even remember cause I forgot to update this. Whoops

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TextManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endLine;
+
+    public Hero player;
+
+    public string nextScene;
+
+    //other classes can trigger this on so that a before textbox action can occur
+    public bool textOnOff = false;
+
+    void Start()
+    {
+        player = FindObjectOfType<Hero>();
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endLine == 0)
+        {
+            endLine = textLines.Length - 1;
+        }
+    }
+
+    void Update()
+    {
+        textBox.SetActive(false);
+
+        if(textOnOff == true)
+        {
+            textBox.SetActive(true);
+
+            if (currentLine < textLines.Length)
+            {
+                theText.text = textLines[currentLine];
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                currentLine += 1;
+            }
+
+            if (currentLine > endLine)
+            {
+                //textBox.SetActive(false);
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/TutorialEnemyEncounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEnemyEncounter : MonoBehaviour
+{
+    public TextManager dialouge;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
